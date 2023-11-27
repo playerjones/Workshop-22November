5f05e9ad1a04878f47d7c40742d9b1273a22a0f0
playerjones <jonasbossen99@hotmail.com>
2023-11-27 10:31:16 +0100

    Commit

--- src/Building.java
+++ src/Building.java
@@ -1,5 +1,24 @@
 package src;
+import java.util.ArrayList;
 
 public class Building {
+    ArrayList Sensors = new ArrayList(1);
+    ArrayList Actuators = new ArrayList(0);
+
+    public void addSensor(String sensors) {
+        Sensors.add(sensors);
+    }
+
+    public void removeSensor(String sensors) {
+        Sensors.remove(sensors);
+    }
+
+    public void addActuator(String actuator) {
+        Actuators.add(actuator);
+    }
+
+    public void removeActuator(String actuator) {
+        Actuators.remove(actuator);
+    }
 
 }
